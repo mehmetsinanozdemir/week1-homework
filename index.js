ecdff6a84690ce77bca269a520b297ad0af19ba2
mehmetsinan ozdemir <mehmet1sinan.2@gmail.com>
2021-07-10 10:02:30 +0300

    eft uygulamasi

--- index.js
+++ index.js
@@ -1,6 +1,83 @@
-import { name } from './example-other-file';
+import userInfo from "./user_information";
 
-console.log('Javascriptinizi buraya yazabilirsiniz')
 
-// Baska dosyadan calistirmak mumkun, dosyalari parcalamaktan korkmayin!
-// name
+var timerElement = document.getElementById("timer");
+
+
+
+
+var mobileInput = document.getElementById("mobileCode");
+var e = document.getElementById("selector");
+var moneyInput =  document.getElementById("AmountInput");
+var submitButton = document.querySelector("#submit");
+var selectedValue = e.options[e.selectedIndex].value;
+var errorCount = 0;
+
+let index = e.selectedIndex;
+if(moneyInput>userInfo.accounts[e].balance){
+   submitButton.disabled=false;
+}
+   else{
+    submitButton.disabled=true;
+}
+   
+
+
+function Submit(){
+   
+    if(selectedValue == "HESAB SECINIZ"){
+        window.alert("Hesab seçiniz")
+    }else{
+        if(moneyInput.value<=500){
+            window.alert("Gönderme başarlı")
+        }
+        else{
+            window.alert("Cep telefonunuza gelen kodu giriniz");
+            mobileInput.style.display = "block";
+                if(mobileInput.innerHTML!="1234"){
+                    errorCount++;
+                    window.alert("Şifre yanlış")
+                    if(errorCount>3){
+                        window.alert("Hesab bloke edildi")
+                    }
+                
+                }else{
+                    window.alert("Başarlı")
+                }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+function timer(duration,timeLabel){
+    let time = duration, minutes, seconds;
+    setInterval(()=>{
+        minutes = parseInt(time / 60, 10);
+        seconds = parseInt(time% 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        console.log(minutes)
+        timeLabel.innerText = minutes + ":" + seconds;
+
+        if (--time < 0) {
+            window.alert("Oturmaniz sonlandirildi")
+            location.reload();
+        }
+    }, 1000);
+}
+timer(2*60,timerElement);
+
+
+
+
+
+
+
---
+++ user_information.js
@@ -0,0 +1,24 @@
+export const accounts = [
+    {
+        
+        iban: "34-1111-7890-1111-4543-56",
+        balance: 4500
+    },
+    {
+       
+        iban: "45-3453-4321-3453-6543-21",
+        balance: 104
+    },
+    {
+        
+        iban: "67-6666-3445-5645-0123-11",
+        balance: 45000
+    }
+  ];
+
+export const user = {
+    name: 'MEHMET',
+    surname: 'SİNAN',
+    accounts
+};
+
